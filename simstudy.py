46fb77c03a3b3ccdc102fb7dbd777007bc1a1733
Yash Deshpande <deshpande.yash@gmail.com>
2019-08-27 00:06:34 +0200

    Static Simple tree simulation added in Simulation class, the method to test it added in Simstudy

--- simstudy.py
+++ simstudy.py
@@ -1,22 +1,49 @@
 from simulation import Simulation
 from matplotlib import pyplot
 
-# Create the simulation object
-sim = Simulation()
-# Seed for repeatability
-#np.random.seed(sim.sim_param.seed)
-# Do the simulation and load the output in arrays
-sim.do_simulation()
-# print throughput
-print("Throughput = " + str(sim.sim_result.throughput))
-print("Mean Packet Delay = " + str(sim.sim_result.mean_packet_delay))
-print("Max packet delay = " + str(sim.sim_result.max_packet_delay))
-print("Mean number of retx = " + str(sim.sim_result.mean_no_tx))
-print("Max packet retx = " + str(sim.sim_result.max_no_tx))
-# Plots for distributions of arrivals and delays
-pyplot.subplot(121)
-pyplot.hist(sim.sim_state.delay_stat_array)
-pyplot.subplot(122)
-pyplot.hist(sim.sim_state.arrival_stat_array)
-pyplot.show()
+
+def simulate_simple_tree_dynamic(sim):
+
+    sim.reset()
+    # Do the simulation
+    sim.do_simulation_simple_tree_dynamic()
+    # print Results
+    print("Throughput = " + str(sim.sim_result.throughput))
+    print("Mean Packet Delay = " + str(sim.sim_result.mean_packet_delay))
+    print("Max packet delay = " + str(sim.sim_result.max_packet_delay))
+    print("Mean number of retx = " + str(sim.sim_result.mean_no_tx))
+    print("Max packet retx = " + str(sim.sim_result.max_no_tx))
+    # Plots for distributions of arrivals and delays
+    pyplot.subplot(121)
+    pyplot.hist(sim.sim_state.delay_stat_array)
+    pyplot.subplot(122)
+    pyplot.hist(sim.sim_state.arrival_stat_array)
+    pyplot.show()
+
+
+def simulate_simple_tree_static(sim):
+    sim.reset()
+    sim.do_simulation_simple_tree_static(30)
+    print("Throughput = " + str(sim.sim_result.throughput))
+    print("Mean Packet Delay = " + str(sim.sim_result.mean_packet_delay))
+    print("Max packet delay = " + str(sim.sim_result.max_packet_delay))
+    print("Mean number of retx = " + str(sim.sim_result.mean_no_tx))
+    print("Max packet retx = " + str(sim.sim_result.max_no_tx))
+    # Plots for distributions of arrivals and delays
+    pyplot.subplot(121)
+    pyplot.hist(sim.sim_state.delay_stat_array)
+    pyplot.subplot(122)
+    pyplot.hist(sim.sim_state.arrival_stat_array)
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    # Create the simulation object
+    sim = Simulation()
+    # Seed for reproducibility
+    # np.random.seed(sim.sim_param.seed)
+    # Comment and uncomment the below methods as it suits
+    #simulate_simple_tree_dynamic(sim)
+    simulate_simple_tree_static(sim)
+
 
--- simulation.py
+++ simulation.py
@@ -29,19 +29,28 @@ class Simulation(object):
         self.active_array = []
         self.slot_array = np.arange(0, self.sim_param.SIMTIME)
 
-    def do_simulation(self):
+    def do_simulation_simple_tree_dynamic(self):
         # Run simulation for the number of slots
         for slot_no in self.slot_array:
             # Generate a packet according to poisson distribution
             packets_gen = np.random.poisson(self.sim_param.lmbda)
             # Add the number of packets to the active packet array
-            active_array = packetlist.add_packets(slot_no, self.sim_state.total_arrivals, packets_gen, self.active_array)
+            self.active_array = packetlist.add_packets(slot_no, self.sim_state.total_arrivals, packets_gen,
+                                                                                                     self.active_array)
             # Simulate the processes that would happen in the tx and rx in one slot, update the active array accordingly
-            self.active_array, result = self.simpletree.oneslotprocess(active_array, printit=False)
+            self.active_array, result = self.simpletree.oneslotprocess(self.active_array, printit=False)
             # Keep track of successes
             self.active_array = self.sim_state.update_metrics(self.active_array, packets_gen, slot_no, result)
         # Update the results
         self.sim_result.get_result(self.sim_state, self.sim_param.SIMTIME)
 
-
-
+    def do_simulation_simple_tree_static(self, collided_packets):
+        # Load active array with the collided packets
+        self.active_array = packetlist.add_packets(0, 0, collided_packets, self.active_array)
+        slot_no = 0
+        while len(self.active_array) != 0:
+            self.active_array, result = self.simpletree.oneslotprocess(self.active_array, printit=False)
+            self.active_array = self.sim_state.update_metrics(self.active_array, 0, slot_no, result)
+            slot_no += 1
+        self.sim_state.total_arrivals = collided_packets
+        self.sim_result.get_result(self.sim_state, slot_no)
