ca15333bd0edc7a4d704083fc9a8bd37ef92cac7
Yash Deshpande <deshpande.yash@gmail.com>
2019-09-24 17:43:25 +0200

    Tree tracking beggining

--- treestate.py
+++ treestate.py
@@ -9,6 +9,8 @@ class TreeState(object):
         self.total_successes = 0
         self.last_slot = 0
         self.result_array = []
+        self.branch_array = []
+        self.branch_status = ''
 
     def reset(self, sim):
         self.first_slot = sim.slot_no
@@ -16,6 +18,8 @@ class TreeState(object):
         self.total_successes = 0
         self.result_array = []
         self.last_slot = 0
+        self.branch_array = []
+        self.branch_status = ''
 
     def update_metrics(self, sim):
         """
@@ -29,6 +33,7 @@ class TreeState(object):
         # Add the number of packets to statistical array for diagnosis
         sim.sim_state.arrival_stat_array.append(sim.packets_gen)
         if sim.result == 1:
+            self.branch_status = self.branch_status + str(sim.sim_param.SPLIT - 1)
             go_on = True
             while go_on:
                 # If the 0 th element of the active array is less than 0, it mans that packet is resolved, hence remove
@@ -44,3 +49,16 @@ class TreeState(object):
                         go_on = False
                 else:
                     go_on = False
+        else:
+            if self.branch_status[-1] == '0':
+                self.branch_status = self.branch_status[:-1]
+            last_branch = self.branch_status[-1]
+            self.branch_status = self.branch_status[:-1]
+            self.branch_status = self.branch_status + str(int(last_branch) - 1)
+
+
+
+
+
+
+
