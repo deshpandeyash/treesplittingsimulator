8c878692ab6c33339550ce1aeb14b10c790b6592
Yash Deshpande <deshpande.yash@gmail.com>
2019-10-16 21:35:42 +0200

    Bug in code while doing tree tracking in Q ary SIC, want to save code before moving on

--- slot.py
+++ slot.py
@@ -39,33 +39,22 @@ class TreeSlot(object):
         # Update the number of transmissions in each packet
         packetlist.update_transmissions(sim)
         # Get the feedback form the receiver
-        feedback, self.resolved_packets = self.rxprocess(sim.sim_param.sic, sim.sim_param.K)
+        feedback, self.resolved_packets = self.rxprocess(sim.sim_param.sic, sim.sim_param.K, sim)
         # If Success
         if feedback == 1:
             # On a success, all other packets reduce their count
             packetlist.dec_packet_count(sim, self.resolved_packets)
             # If SIC process is used, then
-            if sim.sim_param.sic:
+            if sim.sim_param.sic and sim.branch_node.branch_status[-1] == '0':
                 # We increment the count of the uncollided packets
                 packetlist.inc_uncollided_packet_count(sim, 1)
                 # And split the packets which might collide in the next slot
                 packetlist.split_uncollided_packet_count(sim)
                 # Here we update the node of the of the tree according to the result,
-                if sim.slot.resolved_packets > 1:
-                    for _ in range(sim.slot.resolved_packets - 1):
-                        sim.branch_node.next_leaf()
-                    sim.branch_node.split(sim.sim_param.SPLIT)
-                else:
-                    sim.branch_node.next_leaf()
-                    sim.branch_node.split(sim.sim_param.SPLIT)
-            # If not SIC, only one success is registered, so we find the next node
-            else:
-                sim.branch_node.next_leaf()
+                sim.branch_node.split(sim.sim_param.SPLIT)
             sim.result = feedback
         # If Idle
         elif feedback == 0:
-            # On an idle, we find the next leaf
-            sim.branch_node.next_leaf()
             # To identify if the next slot after this idle slot is a definite collision, Modified tree Tree
             self.def_collision = False
             # If modified anf the Simple Tree Result Array has enough elements
@@ -77,8 +66,10 @@ class TreeSlot(object):
                 for k in range(1, sim.sim_param.SPLIT-1):
                     if sim.tree_state.ST_result_array[-k] != 0:
                         self.def_collision = False
+            if sim.sim_param.sic and sim.branch_node.branch_status[-1] == '0':
+                self.def_collision = True
             # If the modified tree algorithm is used, and we have a definite collision
-            if self.def_collision or sim.sim_param.sic:
+            if self.def_collision:
                 # increment the count for uncollided packets
                 packetlist.inc_uncolliding_packet_count(sim, sim.sim_param.SPLIT - 2)
                 # Update the counts on the collided packets according to a binary split
@@ -86,7 +77,7 @@ class TreeSlot(object):
                 # Then split the tree
                 sim.branch_node.split(sim.sim_param.SPLIT)
             else:
-                # if modified tree algorithm is not used,  On an idle slot, all packets reduce their count by 1
+                # On an idle slot, all packets reduce their count by 1 if its not a definite collision
                 packetlist.dec_packet_count(sim, 1)
             sim.result = feedback
         # If Collision
@@ -109,7 +100,7 @@ class TreeSlot(object):
             print("Rx Process did not change give a Feedback")
         sim.branch_node.update_array()
 
-    def rxprocess(self, sic, K):
+    def rxprocess(self, sic, K, sim):
         """
         depending on the active packet array length and the count in each of its packets, this process decides whether
         there was in idle slot, collision or success after which it provides feedback,
@@ -124,12 +115,17 @@ class TreeSlot(object):
         # If the length of the array is 0 then there are no active packets, and no transmissions hence, IDLE
         if len(self.tx_packet_array) == 0:
             feedback = 0
+            # On an idle, we find the next leaf
+            sim.branch_node.next_leaf()
         # If array has just one element and it is 0 then its a success
         elif len(self.tx_packet_array) <= K:
             feedback = 1
             resolved_packets = 1
             if sic:
-                resolved_packets += self.sic_process(K)
+                resolved_packets += self.sic_process(K, sim)
+            # If not SIC, only one success is registered, so we find the next node
+            else:
+                sim.branch_node.next_leaf()
         elif len(self.tx_packet_array) > K:
             feedback = 2
             if sic:
@@ -140,7 +136,7 @@ class TreeSlot(object):
             print("Error, unexpected array")
         return feedback, resolved_packets
 
-    def sic_process(self, K):
+    def sic_process(self, K, sim):
         # The single packet that is decoded, we take its ID
         single_packet = packetlist.extract_packet_id(self.tx_packet_array)
         # Append this ID to the array of already Decoded IDs
@@ -153,23 +149,28 @@ class TreeSlot(object):
             last_coll = self.collided_array[-1]
             # Remove all the pacet IDS that have been resolved
             resolved_array = [x for x in last_coll if x not in self.packetID]
-            # If no packet remains, it means its a left IDLE slot
+            # Save the branch you are coming upwards from
+            last_branch = int(sim.branch_node.branch_status[-1])
+            # move a node up to see the collision
+            sim.branch_node.branch_status = sim.branch_node.branch_status[:-1]
+            # If no packet remains, it means its a left IDLE slots
             if len(resolved_array) == 0:
                 # Delete this collision
                 del self.collided_array[-1]
                 # Increment the feedback
-                sic_resolved_packets += 1
-            # If only one packet remains, this can be resolved too
+                sic_resolved_packets += last_branch
+            # If only K packet remains, this can be resolved too
             elif len(resolved_array) <= K:
                 # Delete this collision
                 del self.collided_array[-1]
                 # Add the resolved packet to the packet IDS tht have already been resolved
                 self.packetID.extend(resolved_array)
-                sic_resolved_packets += 1
+                sic_resolved_packets += last_branch
             # This collision cannot be resolved
             else:
                 # We still remove only the resolved packets from the last collision, and stop the SIC process
                 del self.collided_array[-1]
                 self.collided_array.append(resolved_array)
+                sim.branch_node.branch_status = sim.branch_node.branch_status + str(last_branch - 1)
                 go_on = False
         return sic_resolved_packets
