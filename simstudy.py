05496339348b4810c5568e25da62b7c6e084a4af
deshpandeyash <yash.deshpande@tum.de>
2020-04-14 13:32:50 +0200

    changing osc test to include length and n start at one

--- simstudy.py
+++ simstudy.py
@@ -327,39 +327,39 @@ def experimental_runs(sim, setting, date_time_folder, txt_context):
     """
 
     # ------------------------------------ For the Large Oscillation Plot -----------------------------------
-    # start = time.time()
-    # k_array = [1, 2, 6, 8, 16]
-    # multiple_theoretical = []
-    # maximum_array = []
-    # n_array = []
-    # n_stop = 1000
-    # for k in k_array:
-    #     sim.sim_param.K = k
-    #     # user_array = np.arange(sim.sim_param.K + 1, n_stop)
-    #     user_array = np.arange(1, n_stop)
-    #     theoretical = []
-    #     for n in user_array:
-    #         # theoretical.append(TheoreticalPlots().qarysic(n, sim.sim_param))
-    #         theoretical.append(TheoreticalPlots().qarylen(n, sim.sim_param))
-    #     multiple_theoretical.append(theoretical)
-    #     maximum_array.append(max(theoretical))
-    #     n_array.append(user_array[theoretical.index(max(theoretical))])
-    #     pyplot.plot(user_array, theoretical, label=f"K = {k}")
-    # pyplot.plot(n_array, maximum_array, 'r--', label='Maximum')
-    # print(F"N array is {n_array}")
-    # # pyplot.xscale('log')
-    # # pyplot.yscale('log')
-    # pyplot.legend()
-    # pyplot.xlabel('Users')
-    # pyplot.ylabel('Throughput')
-    # pyplot.grid()
-    # figname = date_time_folder + f"Q{sim.sim_param.SPLIT}allKplotsp"
-    # pyplot.savefig(figname + '.png', dpi=300)
-    # tikzplotlib.save(figname + '.tex', encoding='utf-8')
-    #
-    # pyplot.show()
-    # end = time.time()
-    # print("Time for Simulations = " + str(end-start))
+    start = time.time()
+    k_array = [1, 2, 4, 8, 16]
+    multiple_theoretical = []
+    maximum_array = []
+    n_array = []
+    n_stop = 1000
+    for k in k_array:
+        sim.sim_param.K = k
+        user_array = np.arange(sim.sim_param.K + 1, n_stop)
+        # user_array = np.arange(1, n_stop)
+        theoretical = []
+        for n in user_array:
+            theoretical.append(TheoreticalPlots().qarysic(n, sim.sim_param))
+            # theoretical.append(TheoreticalPlots().qarylen(n, sim.sim_param))
+        multiple_theoretical.append(theoretical)
+        maximum_array.append(max(theoretical))
+        n_array.append(user_array[theoretical.index(max(theoretical))])
+        pyplot.plot(user_array, theoretical, label=f"K = {k}")
+    pyplot.plot(n_array, maximum_array, 'r--', label='Maximum')
+    print(F"N array is {n_array}")
+    pyplot.xscale('log')
+    # pyplot.yscale('log')
+    pyplot.legend()
+    pyplot.xlabel('Users')
+    pyplot.ylabel('Throughput')
+    pyplot.grid()
+    figname = date_time_folder + f"Q{sim.sim_param.SPLIT}allKplotsp"
+    pyplot.savefig(figname + '.png', dpi=300)
+    tikzplotlib.save(figname + '.tex', encoding='utf-8')
+
+    pyplot.show()
+    end = time.time()
+    print("Time for Simulations = " + str(end-start))
 
     # ----------------- For the plot that shows optimal pj --------------------------------------------------
     # start = time.time()
@@ -435,90 +435,90 @@ def experimental_runs(sim, setting, date_time_folder, txt_context):
     # print(F"Time for simulation is {end-start}")
     # _______________________ Bound Plots ____________________________________________
 
-    start = time.time()
-    k_array = [1, 2, 4, 8, 16]
-    m = 50
-    bounds_table = pd.DataFrame()
-    alpha_array_bound = []
-    beta_array_bound = []
-    alpha_k_array_bound = []
-    beta_k_array_bound = []
-    lambda_lower_array_bound = []
-    lambda_upper_array_bound = []
-    lambda_delta_array_bound = []
-    delta_array_bound = []
-
-    print(F"m = {m} ")
-    for k in k_array:
-        sim.sim_param.K = k
-        n_array = np.arange(m+1, m+500)
-        alpha_plot = []
-        for n in n_array:
-            numerator = 0
-            denominator = 0
-            for i in range(0, m):
-                li = TheoreticalPlots().qarylen(i, sim.sim_param)
-                comber = comb(n, i, exact=True)
-                numerator += comber * (li + 1)
-                denominator += comber * i
-            alpha_plot.append(numerator/denominator)
-        alpha_lb = min(alpha_plot)
-        alpha_ub = max(alpha_plot)
-        print(F"............................................................")
-        print(F"For k = {k} ")
-        print(F"Lower Bound {alpha_lb:.7f} and Upper Bound = {alpha_ub:.7f}")
-        print(F"Normalizing with K")
-        print(F"Lower Bound {alpha_lb*k:.7f} and Upper Bound {alpha_ub*k:.7f}")
-
-        alpha_array_bound.append(round(float(alpha_lb), 6))
-        beta_array_bound.append(round(float(alpha_ub), 6))
-        alpha_k_array_bound.append(round(float(alpha_lb*k), 6))
-        beta_k_array_bound.append(round(float(alpha_ub*k), 6))
-
-        # Now onto the Windowed Access Results
-        lambda_upper_array = []
-        lambda_lower_array = []
-        lambda_delta_array = np.linspace(0.1, 25, 2500)
-        for lambda_delta in lambda_delta_array:
-            lambda_upper_array.append(lambda_delta/float(TheoreticalPlots().windowed_bound(sim.sim_param, alpha_ub, m,
-                                                                                           lambda_delta)))
-            lambda_lower_array.append(lambda_delta/float(TheoreticalPlots().windowed_bound(sim.sim_param, alpha_lb, m,
-                                                                                           lambda_delta)))
-        lambda_upper_array = np.asarray(lambda_upper_array) / k
-        lambda_lower_array = np.asarray(lambda_lower_array) / k
-        lambda_upper = max(lambda_upper_array)
-        lambda_lower = max(lambda_lower_array)
-        arg_index = np.argmax(lambda_upper_array)
-        optimum_lambda_delta = lambda_delta_array[arg_index]
-        optimum_window = optimum_lambda_delta / lambda_upper
-        pyplot.plot(lambda_delta_array, lambda_upper_array, label=F"K{k}")
-        print(F"Lower Bound on Lambda is {lambda_lower:.7f} and upper bound is {lambda_upper:.7f}")
-        print(F"Optimum lambda-Delta is {optimum_lambda_delta} and optimum window size is {optimum_window}")
-        lambda_lower_array_bound.append(round(float(lambda_lower), 6))
-        lambda_upper_array_bound.append(round(float(lambda_upper), 6))
-        lambda_delta_array_bound.append(round(float(optimum_lambda_delta), 6))
-        delta_array_bound.append(round(float(optimum_window), 6))
-    pyplot.xlabel("Lambda_Delta")
-    pyplot.ylabel("Lambda")
-    pyplot.legend()
-    pyplot.grid()
-    figname = date_time_folder + f"WindowedAccessPLots"
-    pyplot.savefig(figname + '.png', dpi=300)
-    tikzplotlib.save(figname + '.tex', encoding='utf-8')
-    pyplot.show()
-    bounds_table['K'] = k_array
-    bounds_table['alpha'] = alpha_array_bound
-    bounds_table['beta'] = beta_array_bound
-    bounds_table['alphaK'] = alpha_k_array_bound
-    bounds_table['betaK'] = beta_k_array_bound
-    bounds_table['lambdaUpper'] = lambda_upper_array_bound
-    bounds_table['lambdaLower'] = lambda_lower_array_bound
-    bounds_table['lambdaDelta'] = lambda_delta_array_bound
-    bounds_table['Delta'] = delta_array_bound
-    with open(figname + 'table.tex', 'w') as tf:
-        tf.write(bounds_table.to_latex())
-    end = time.time()
-    print(F"Time for Simulation is {end-start}")
+    # start = time.time()
+    # k_array = [1, 2, 4, 8, 16]
+    # m = 50
+    # bounds_table = pd.DataFrame()
+    # alpha_array_bound = []
+    # beta_array_bound = []
+    # alpha_k_array_bound = []
+    # beta_k_array_bound = []
+    # lambda_lower_array_bound = []
+    # lambda_upper_array_bound = []
+    # lambda_delta_array_bound = []
+    # delta_array_bound = []
+    #
+    # print(F"m = {m} ")
+    # for k in k_array:
+    #     sim.sim_param.K = k
+    #     n_array = np.arange(m+1, m+500)
+    #     alpha_plot = []
+    #     for n in n_array:
+    #         numerator = 0
+    #         denominator = 0
+    #         for i in range(0, m):
+    #             li = TheoreticalPlots().qarylen(i, sim.sim_param)
+    #             comber = comb(n, i, exact=True)
+    #             numerator += comber * (li + 1)
+    #             denominator += comber * i
+    #         alpha_plot.append(numerator/denominator)
+    #     alpha_lb = min(alpha_plot)
+    #     alpha_ub = max(alpha_plot)
+    #     print(F"............................................................")
+    #     print(F"For k = {k} ")
+    #     print(F"Lower Bound {alpha_lb:.7f} and Upper Bound = {alpha_ub:.7f}")
+    #     print(F"Normalizing with K")
+    #     print(F"Lower Bound {alpha_lb*k:.7f} and Upper Bound {alpha_ub*k:.7f}")
+    #
+    #     alpha_array_bound.append(round(float(alpha_lb), 6))
+    #     beta_array_bound.append(round(float(alpha_ub), 6))
+    #     alpha_k_array_bound.append(round(float(alpha_lb*k), 6))
+    #     beta_k_array_bound.append(round(float(alpha_ub*k), 6))
+    #
+    #     # Now onto the Windowed Access Results
+    #     lambda_upper_array = []
+    #     lambda_lower_array = []
+    #     lambda_delta_array = np.linspace(0.1, 25, 2500)
+    #     for lambda_delta in lambda_delta_array:
+    #         lambda_upper_array.append(lambda_delta/float(TheoreticalPlots().windowed_bound(sim.sim_param, alpha_ub, m,
+    #                                                                                        lambda_delta)))
+    #         lambda_lower_array.append(lambda_delta/float(TheoreticalPlots().windowed_bound(sim.sim_param, alpha_lb, m,
+    #                                                                                        lambda_delta)))
+    #     lambda_upper_array = np.asarray(lambda_upper_array) / k
+    #     lambda_lower_array = np.asarray(lambda_lower_array) / k
+    #     lambda_upper = max(lambda_upper_array)
+    #     lambda_lower = max(lambda_lower_array)
+    #     arg_index = np.argmax(lambda_upper_array)
+    #     optimum_lambda_delta = lambda_delta_array[arg_index]
+    #     optimum_window = optimum_lambda_delta / lambda_upper
+    #     pyplot.plot(lambda_delta_array, lambda_upper_array, label=F"K{k}")
+    #     print(F"Lower Bound on Lambda is {lambda_lower:.7f} and upper bound is {lambda_upper:.7f}")
+    #     print(F"Optimum lambda-Delta is {optimum_lambda_delta} and optimum window size is {optimum_window}")
+    #     lambda_lower_array_bound.append(round(float(lambda_lower), 6))
+    #     lambda_upper_array_bound.append(round(float(lambda_upper), 6))
+    #     lambda_delta_array_bound.append(round(float(optimum_lambda_delta), 6))
+    #     delta_array_bound.append(round(float(optimum_window), 6))
+    # pyplot.xlabel("Lambda_Delta")
+    # pyplot.ylabel("Lambda")
+    # pyplot.legend()
+    # pyplot.grid()
+    # figname = date_time_folder + f"WindowedAccessPLots"
+    # pyplot.savefig(figname + '.png', dpi=300)
+    # tikzplotlib.save(figname + '.tex', encoding='utf-8')
+    # pyplot.show()
+    # bounds_table['K'] = k_array
+    # bounds_table['alpha'] = alpha_array_bound
+    # bounds_table['beta'] = beta_array_bound
+    # bounds_table['alphaK'] = alpha_k_array_bound
+    # bounds_table['betaK'] = beta_k_array_bound
+    # bounds_table['lambdaUpper'] = lambda_upper_array_bound
+    # bounds_table['lambdaLower'] = lambda_lower_array_bound
+    # bounds_table['lambdaDelta'] = lambda_delta_array_bound
+    # bounds_table['Delta'] = delta_array_bound
+    # with open(figname + 'table.tex', 'w') as tf:
+    #     tf.write(bounds_table.to_latex())
+    # end = time.time()
+    # print(F"Time for Simulation is {end-start}")
 
 def make_result_folder():
     today = date.today()
