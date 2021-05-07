a66d2f6015df23399c6e3550b97a7c4456e53df2
deshpandeyash <yash.deshpande@tum.de>
2021-05-07 11:00:16 +0200

    complex analysis in the optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,23 +1,31 @@
 import numpy as np
-import math
 from matplotlib import pyplot as plt
 from decimal import Decimal as dc
+from scipy.special import gamma
+import math
+import tikzplotlib
 
-k_array = np.arange(21)
+
+def return_A(K, m):
+    imag_comp = (2j * math.pi * m) / math.log(2)
+    outer_k_sum = 0
+    for k in range(1, K + 1):
+        inner_prod = [i - 1 + imag_comp for i in range(0, k)]
+        prod = np.prod(np.asarray(inner_prod))
+        outer_k_sum += prod / math.factorial(k)
+    return 1 + outer_k_sum
+
+
 m = 1
-
-def get_A(k,m):
-    complex_add = 2j*math.pi / math.log(2)
-    print(complex_add)
-    k_sum = []
-    for k_i in range(1,k+1):
-        prod_array = []
-        for i in range(k_i):
-            prod_array.append(complex((i - 1) + complex_add))
-        prod = np.prod(np.asarray(prod_array))
-        k_sum.append(prod/math.factorial(k_i))
-    return 1 + sum(k_sum)
-
-plot_array = [get_A(a,m) for a in k_array]
-plt.plot(k_array,plot_array)
+k_range = np.arange(1, 101)
+B_array = np.asarray([abs(gamma(1 + (2j * math.pi * m) / math.log(2)) * return_A(k, m)) for k in k_range])
+plt.plot(k_range, B_array)
+max_value = np.max(B_array)
+ind_at_max = k_range[np.argmax(B_array)]
+plt.vlines(ind_at_max,0,max_value, colors='red', linestyles='dashed')
+plt.xlabel("K")
+plt.ylabel(F"|B(K,1)|")
+plt.tight_layout()
+tikzplotlib.save(F"ComplexAnalysis", encoding="UTF8")
+print(max_value)
 plt.show()
